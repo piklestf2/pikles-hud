6a311fddcf311f0eb37ad48c5d2930c0787641d1
piklestf2 <piklestf2@users.noreply.github.com>
2022-12-06 11:22:49 -0800

    Move tf_english.txt patch file to root

---
+++ patch_tf_english.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+patch -o resource/tf_english.txt ../../resource/tf_english.txt resource/tf_english_playerid.patch
--- resource/patch_tf_english.sh
+++
@@ -1,2 +0,0 @@
-#!bin/sh
-patch -o tf_english.txt ../../../resource/tf_english.txt tf_english_playerid.patch
